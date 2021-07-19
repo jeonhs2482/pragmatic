800bd17583dd96be190194ab055dfaaefc0902b4
jeonhs2482 <66204827+jeonhs2482@users.noreply.github.com>
2021-07-19 18:38:03 +0900

    ADD: login/logout view

--- accountapp/views.py
+++ accountapp/views.py
@@ -29,5 +29,5 @@ def hello_world(request):
 class AccountCreateView(CreateView):
     model         = User
     form_class    = UserCreationForm
-    success_url   = reverse_lazy('accountapp:create')
+    success_url   = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
--- pinterest/settings.py
+++ pinterest/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib     import Path
+
+from django.urls.base import reverse_lazy
 from my_settings import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -131,3 +133,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
