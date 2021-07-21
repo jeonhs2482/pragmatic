ead542767da2e2834901ab2cd3b8505c015777d0
jeonhs2482 <66204827+jeonhs2482@users.noreply.github.com>
2021-07-21 19:41:31 +0900

    ADD: commentapp

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,10 +1,11 @@
-from django.views.generic.edit      import DeleteView
-from django.views.generic           import CreateView, UpdateView
+from django.views.generic.edit      import DeleteView, FormMixin
+from django.views.generic           import CreateView, UpdateView, ListView
 from django.utils.decorators        import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.urls                    import reverse, reverse_lazy
 from django.views.generic.detail    import DetailView
 
+from commentapp.forms               import CommentCreationForm
 from articleapp.forms               import ArticleCreationForm
 from articleapp.models              import Article
 from articleapp.decorators          import article_ownership_required
@@ -28,8 +29,9 @@ class ArticleCreateView(CreateView):
         return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(DetailView, FormMixin):
     model               = Article
+    form_class          = CommentCreationForm
     context_object_name = 'target_article'
     template_name       = 'articleapp/detail.html'
 
@@ -52,4 +54,12 @@ class ArticleDeleteView(DeleteView):
     context_object_name = 'target_article'
     success_url         = reverse_lazy('articleapp:list')
     template_name       = 'articleapp/delete.html'
+
+
+class ArticleListView(ListView):
+    model               = Article
+    context_object_name = 'article_list'
+    template_name       = 'articleapp/list.html'
+    paginate_by         = 2
+
 
--- pinterest/settings.py
+++ pinterest/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'bootstrap4',
     'profileapp',
     'articleapp',
+    'commentapp',
 ]
 
 MIDDLEWARE = [
