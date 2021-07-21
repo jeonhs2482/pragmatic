7f4b2bd09ac238ab84691785ec9c0bd60cac125a
jeonhs2482 <66204827+jeonhs2482@users.noreply.github.com>
2021-07-21 15:47:54 +0900

    ADD: articleapp

---
+++ articleapp/models.py
@@ -0,0 +1,12 @@
+from django.db                  import models
+from django.contrib.auth.models import User
+
+
+class Article(models.Model): 
+    writer     = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
+    title      = models.CharField(max_length=200, null=True)
+    image      = models.ImageField(upload_to='article/', null=False)
+    content    = models.TextField(null=True)
+    created_ad = models.DateField(auto_now_add=True, null=True)
+
+
--- pinterest/settings.py
+++ pinterest/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'accountapp',
     'bootstrap4',
     'profileapp',
+    'articleapp',
 ]
 
 MIDDLEWARE = [
