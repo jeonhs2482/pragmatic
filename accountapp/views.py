0dfb2884f9c7770be30568946b587d8901e78f9b
jeonhs2482 <66204827+jeonhs2482@users.noreply.github.com>
2021-07-22 22:14:00 +0900

    MODIFY: accountapp/views.py

--- accountapp/views.py
+++ accountapp/views.py
@@ -17,7 +17,7 @@ has_ownership = [account_ownership_required, login_required]
 class AccountCreateView(CreateView):
     model         = User
     form_class    = UserCreationForm
-    success_url   = reverse_lazy('accountapp:create')
+    success_url   = reverse_lazy('articleapp:list')
     template_name = 'accountapp/create.html'
 
 class AccountDetailView(DetailView, MultipleObjectMixin):
