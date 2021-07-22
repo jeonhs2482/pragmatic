27c025f95d405b7d3b76db9c72c1c28e9a92fbe9
jeonhs2482 <66204827+jeonhs2482@users.noreply.github.com>
2021-07-22 16:00:25 +0900

    MODIFY: icon modify

--- accountapp/views.py
+++ accountapp/views.py
@@ -8,31 +8,11 @@ from django.contrib.auth.forms      import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.views.generic.list      import MultipleObjectMixin
 
-from accountapp.models              import HelloWorld
 from accountapp.forms               import AccountUpdateForm
 from accountapp.decorators          import account_ownership_required
 from articleapp.models              import Article
 
 has_ownership = [account_ownership_required, login_required]
-
-@login_required
-def hello_world(request):
-
-    if request.method == "POST":
-
-        temp = request.POST.get('hello_world_input')
-
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
-
-        hello_world_list = HelloWorld.objects.all()
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
-    else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
-
-
 
 class AccountCreateView(CreateView):
     model         = User
